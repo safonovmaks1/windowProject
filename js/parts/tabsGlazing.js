c0eaecd4a88613b4e5a25091e2f156a0d83ba493
Safonov Maks <safonovmaks1@yandex.ru>
2019-03-29 22:02:50 +0300

    tabs

--- js/parts/tabsGlazing.js
+++ js/parts/tabsGlazing.js
@@ -1,39 +1,6 @@
 function tabsGlazing() {
 
-    let tab = document.querySelectorAll('.glazing_block'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.glazing_slider');
-
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove('show');
-            tabContent[i].classList.add('hide');
-        }
-    }
-    hideTabContent(1);
-
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains('hide')) {
-            hideTabContent(0);
-            tabContent[b].classList.remove('hide');
-            tabContent[b].classList.add('active');
-        }
-    }
-
-    info.addEventListener('click', (event) => {
-        let target = event.target;
-
-        if (target && target.classList.contains('info-header-tab')) {
-            for (let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
-                }
-            }
-        }
-    });
-
+    
 }
 
 module.exports = tabsGlazing;
